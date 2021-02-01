5e1df775ee680538d6fec4e1f6bb2fa373fa11fb
nufa flufa <nofargabso@gmail.com>
2021-02-01 16:11:01 +0200

    added info-bank and spec-render

---
+++ js/info-bank.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var gSpecialities = [
+    {
+        header:'Dog Training',
+        txt:'I have been training dogs for 10 years. In my military service I was a combat soldier in the K9 unit, training dogs to find explosive while training obstacle course and discipline.',
+        imgURL:'img/spec/IMG_1195'
+
+    },
+    {
+        header:'Pasta Expert',
+        txt:'After working with pasta for 4 and a half years, I gathered the knowledge and understanding of this special world. different types, different sauces and unique styles of dishes are some of the things I met during those years.',
+        imgURL: 'img/spec/IMG_1194'
+
+    },
+    {
+        header:'Crossfit Training',
+        txt:'Crossfit training has been one of my daily routines over the last three years. my stronger aspect in the sport is weight-lifting, yet I work to excel at all aspects of the sports.',
+        imgURL: 'img/spec/IMG_1193'
+
+    },
+]
--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('Starting up');
+
+
+$(document).ready(init)
+
+function init(){
+    console.log('Starting up');
+    renderSpecialities()
+}
+
+function renderSpecialities(){
+    var strHTMLs = gSpecialities.map(function(spec){
+    return `<div class="col-md-4">
+            <span class="fa-stack fa-4x">
+            <i class="fa fa-circle fa-stack-2x text-primary"></i>
+            <img src="${spec.imgURL}" class="fa fa-stack-1x fa-inverse" style="border-radius: 60px;">
+            </span>
+          <h4 class="service-heading">${spec.header}</h4>
+          <p class="text-muted">${spec.txt}</p>
+        </div>`
+    }) 
+    console.log(strHTMLs)
+    $('.spec-container').html(strHTMLs.join(''));
+}
